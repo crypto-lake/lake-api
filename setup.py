40ecbfd4ceb711fc460fbd0f7a708f1d7441997c
Jan Škoda <jan.skoda@quantlane.com>
2022-10-08 21:46:00 +0200

    fix ci

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     description="API for accessing Lake crypto market data",
     entry_points={
