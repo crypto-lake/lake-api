ffe091ac03177aeb01884408851bc22bb1060d40
Jan Škoda <jan.skoda@quantlane.com>
2022-10-09 19:31:33 +0200

    Bump version: 0.1.0 → 0.1.1

--- lakeapi/__init__.py
+++ lakeapi/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Jan Skoda"""
 __email__ = "skoda@jskoda.cz"
-__version__ = "0.1.0"
+__version__ = "__version__ = '0.1.1'"
 
 from .main import load_data, set_default_bucket, use_sample_data # noqa
