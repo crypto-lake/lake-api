a7106fb2b6a7b54070c2fe29ece30a3968624012
Jan Škoda <jan.skoda@quantlane.com>
2022-11-04 11:05:29 +0100

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/leftys/lakeapi',
-    version='0.2.0',
+    version='0.3.0',
     zip_safe=False,
 )
