4e2e70a8e3e00c554f819472ad2b9dccfb0a975f
Jan Škoda <jan.skoda@quantlane.com>
2022-12-09 10:32:05 +0100

    Bump version: 0.4.3 → 0.4.4

--- lakeapi/__init__.py
+++ lakeapi/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Jan Skoda"""
 __email__ = "skoda@jskoda.cz"
-__version__ = "__version__ = '0.4.3'"
+__version__ = "__version__ = '0.4.4'"
 
 from .main import load_data, list_data, available_symbols, set_default_bucket, use_sample_data, cache # noqa
--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/crypto-lake/lakeapi',
-    version='0.4.3',
+    version='0.4.4',
     zip_safe=False,
 )
