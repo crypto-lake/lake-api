467d0873f8bd2569c90a0e5081ad2d333f243477
Jan Škoda <jan.skoda@quantlane.com>
2023-09-25 18:38:27 +0200

    Bump version: 0.7.0 → 0.8.0

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/crypto-lake/lake-api',
-    version='0.7.0',
+    version='0.8.0',
     zip_safe=False,
 )
