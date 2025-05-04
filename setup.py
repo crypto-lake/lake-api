d65b9015fb3d097e065c5cdb5595c06ebb802016
Jan Škoda <skoda@jskoda.cz>
2025-05-04 21:35:00 +0200

    Bump version: 0.20.1 → 0.21.0

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/crypto-lake/lake-api',
-    version='0.20.1',
+    version='0.21.0',
     zip_safe=False,
 )
